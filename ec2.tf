74c120c4fe6021fcc861670404530b0acbed8632
hiram-rosales <hiram.rosales@globant.com>
2022-07-24 12:40:24 -0500

    Adding dataSources

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,17 @@ provider "aws" {
   region     = var.region
 }
 
+data "aws_ami" "my_ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm"]
+  }
+}
+
 resource "aws_instance" "terraform_cloud" {
-    ami = "ami-0022f774911c1d690"
+    ami = data.aws_ami.my_ami
     instance_type = "t2.micro"
 }
